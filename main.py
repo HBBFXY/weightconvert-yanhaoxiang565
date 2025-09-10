5486761e94133fb70b028e1a6ec19ddfcd699be1
yanhaoxiang565 <ljm060412@qq.com>
2025-09-10 19:40:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # 获取用户输入的重量及单位
-weight_input = input()
+weight_input = 输入()
 # 提取数字部分和单位部分
-num = float(weight_input[:-2])
+num = 漂浮(weight_input[:-2])
 unit = weight_input[-2:]
 # 定义转换系数
 kg_to_pd = 2.2046
-if unit == "kg":
+如果 unit == "kg":
     # 千克转磅
     result = num * kg_to_pd
-    print(f"对应的英制重量为{result:.3f}磅")
-elif unit == "pd":
+    打印(f"对应的英制重量为{result:.3f}磅")
+Elif unit == "pd":
     # 磅转千克
     result = num / kg_to_pd
-    print(f"对应的公制重量为{result:.3f}公斤")
+    打印(f"对应的公制重量为{result:.3f}公斤")
 
