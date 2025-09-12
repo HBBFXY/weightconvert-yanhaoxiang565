c16a4bcba4b89a5c2d3a8e04baee8962010b4d17
yanhaoxiang565 <ljm060412@qq.com>
2025-09-12 11:13:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-weight = input().strip()
-unit = weight[-2:].lower()  # 提取单位并转小写，适配大小写
-num_part = weight[:-2]
-
-try:
-    num = float(num_part)
-    if unit == "kg":
-        pound = num * 2.2046
-        print(f"对应的英制重量为{pound:.3f}磅")
-    elif unit == "pd":
-        kilogram = num / 2.2046
-        print(f"对应的公制重量为{kilogram:.3f}公斤")
-    else:
-        print("输入格式错误")
-except (ValueError, IndexError):
+weight_input=input()
+if weight_input[-2:] in ["kg","KG"]: 
+    pound=(eval(weight_input[0:-2]))*2.2046
+    print("对应的英制重量为{:.3f}磅".format(pound))
+elif weight_input[-2:] in ["pd","PD"]:
+    kilogram=(eval(weight_input[0:-2]))*0.4535
+    print("对应的公制重量为{:.3f}公斤".format(kilogram))
+else:
     print("输入格式错误")
