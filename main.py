66e57a94170e5a10b1de6eb21dcfdabe19ac2d89
yanhaoxiang565 <ljm060412@qq.com>
2025-09-12 10:46:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ try:
     if unit == "kg":
         pound = num * 2.2046
         print(f"对应的英制重量为{pound:.3f}磅")
-    elif unit == "pd":
+    elif unit == "pd" or unit == "lb":
         kilogram = num / 2.2046
         print(f"对应的公制重量为{kilogram:.3f}公斤")
     else:
