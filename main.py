e27b168c86f0a9c15301d1b0f4c3359dfac08768
yanhaoxiang565 <ljm060412@qq.com>
2025-09-12 10:23:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #weightConvert.py
 weight_str=input()
 if weight_str[-2:]=="kg":
-    pound=float(weight_str[0:-2])*2.2046
+    pound=float(weight_str[:-2])*2.2046
     print("对应的英制重量为{: .3f}磅".format(pound))
 elif weight_str[-2:]=="pd":
-    kilogram=float(weight_str[0:-2])/2.2046
+    kilogram=float(weight_str[:-2])/2.2046
     print("对应的公制重量为{: .3f}公斤".format(kilogram))
 else:
     print("输入格式错误")
